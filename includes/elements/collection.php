5c1c07fc5ac9d38276b0e04cea8e5b8d590ea064
Mati Horovitz <mati@pojo.me>
2018-10-25 19:55:37 +0300

    Collection: New - Set columns via grid.

--- includes/elements/collection.php
+++ includes/elements/collection.php
@@ -51,8 +51,8 @@ class Element_Collection extends Element_Container {
 			[
 				'label' => __( 'Columns', 'elementor-pro' ),
 				'type' => Controls_Manager::SELECT,
-				'default' => '3',
-				'tablet_default' => '2',
+				'default' => '1',
+				'tablet_default' => '1',
 				'mobile_default' => '1',
 				'options' => [
 					'1' => '1',
@@ -64,6 +64,60 @@ class Element_Collection extends Element_Container {
 				],
 				'prefix_class' => 'elementor-grid%s-',
 				'frontend_available' => true,
+			]
+		);
+
+		$this->end_controls_section();
+
+		$this->start_controls_section(
+			'section_design_layout',
+			[
+				'label' => __( 'Layout', 'elementor-pro' ),
+				'tab' => Controls_Manager::TAB_STYLE,
+			]
+		);
+
+		$this->add_control(
+			'column_gap',
+			[
+				'label' => __( 'Columns Gap', 'elementor-pro' ),
+				'type' => Controls_Manager::SLIDER,
+				'default' => [
+					'size' => 30,
+				],
+				'range' => [
+					'px' => [
+						'min' => 0,
+						'max' => 100,
+					],
+				],
+				'selectors' => [
+					'{{WRAPPER}} .elementor-collection-container' => 'grid-column-gap: {{SIZE}}{{UNIT}}',
+					'.elementor-msie {{WRAPPER}} .elementor-collection-item' => 'padding-right: calc( {{SIZE}}{{UNIT}}/2 ); padding-left: calc( {{SIZE}}{{UNIT}}/2 );',
+					'.elementor-msie {{WRAPPER}} .elementor-collection-container' => 'margin-left: calc( -{{SIZE}}{{UNIT}}/2 ); margin-right: calc( -{{SIZE}}{{UNIT}}/2 );',
+				],
+			]
+		);
+
+		$this->add_control(
+			'row_gap',
+			[
+				'label' => __( 'Rows Gap', 'elementor-pro' ),
+				'type' => Controls_Manager::SLIDER,
+				'default' => [
+					'size' => 35,
+				],
+				'range' => [
+					'px' => [
+						'min' => 0,
+						'max' => 100,
+					],
+				],
+				'frontend_available' => true,
+				'selectors' => [
+					'{{WRAPPER}} .elementor-collection-container' => 'grid-row-gap: {{SIZE}}{{UNIT}}',
+					'.elementor-msie {{WRAPPER}} .elementor-collection-item' => 'padding-bottom: {{SIZE}}{{UNIT}};',
+				],
 			]
 		);
 
@@ -95,7 +149,7 @@ class Element_Collection extends Element_Container {
 			$this->print_shape_divider( 'bottom' );
 		}
 		?>
-		<div class="elementor-container elementor-collection elementor-grid">
+		<div class="elementor-container elementor-collection-container elementor-grid">
 		<?php
 	}
 
@@ -130,7 +184,7 @@ class Element_Collection extends Element_Container {
 		<div class="elementor-background-overlay"></div>
 		<div class="elementor-shape elementor-shape-top"></div>
 		<div class="elementor-shape elementor-shape-bottom"></div>
-		<div class="elementor-container elementor-collection elementor-grid">
+		<div class="elementor-container elementor-collection-container elementor-grid">
 			<#
 			var items = Math.max( 1, settings.items );
 			for ( var x in [...Array( items ).keys()] ) {#>

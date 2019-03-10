0a364fb0b1dd1818dac0fc51c69157c364468820
Mati Horovitz <mati@pojo.me>
2019-03-10 18:17:58 +0200

    Document: Progress.

--- assets/dev/js/editor/document.js
+++ assets/dev/js/editor/document.js
@@ -50,6 +50,14 @@ var History = {
 		} );
 	},
 
+	settings( args, target ) {
+		elementor.history.history.startItem( {
+			type: 'change',
+			title: 1 === target.getSelection().length ? this.getModelLabel( args[ 0 ] ) : 'Elements',
+			elementType: args[ 0 ],
+		} );
+	},
+
 	moveTo( args, target ) {
 		let title;
 
@@ -64,6 +72,99 @@ var History = {
 			type: 'move',
 			title: title,
 		} );
+	},
+
+	saveHistory: function( model, options ) {
+		if ( ! elementor.history.history.getActive() ) {
+			return;
+		}
+
+		var self = this,
+			changed = Object.keys( model.changed ),
+			control = model.controls[ changed[ 0 ] ];
+
+		if ( ! control && options && options.control ) {
+			control = options.control;
+		}
+
+		if ( ! changed.length || ! control ) {
+			return;
+		}
+
+		if ( 1 === changed.length ) {
+			if ( _.isUndefined( self.oldValues[ control.name ] ) ) {
+				self.oldValues[ control.name ] = model.previous( control.name );
+			}
+
+			if ( elementor.history.history.isItemStarted() ) {
+				// Do not delay the execution
+				self.saveTextHistory( model, changed, control );
+			} else {
+				self.lazySaveTextHistory( model, changed, control );
+			}
+
+			return;
+		}
+
+		var changedAttributes = {};
+
+		_.each( changed, function( controlName ) {
+			changedAttributes[ controlName ] = {
+				old: model.previous( controlName ),
+				new: model.get( controlName ),
+			};
+		} );
+
+		var historyItem = {
+			type: 'change',
+			elementType: 'control',
+			title: elementor.history.history.getModelLabel( model ),
+			history: {
+				behavior: this,
+				changed: changedAttributes,
+				model: this.view.getEditModel().toJSON(),
+			},
+		};
+
+		if ( 1 === changed.length ) {
+			historyItem.subTitle = control.label;
+		}
+
+		elementor.history.history.addItem( historyItem );
+	},
+
+	restore: function( historyItem, isRedo ) {
+		var	history = historyItem.get( 'history' ),
+			modelID = history.model.id,
+			view = elementor.history.history.findView( modelID );
+
+		if ( ! view ) {
+			return;
+		}
+
+		var model = view.getEditModel ? view.getEditModel() : view.model,
+			settings = model.get( 'settings' ),
+			behavior = view.getBehavior( 'ElementHistory' );
+
+		// Stop listen to restore actions
+		behavior.stopListening( settings, 'change', this.saveHistory );
+
+		var restoredValues = {};
+		_.each( history.changed, function( values, key ) {
+			if ( isRedo ) {
+				restoredValues[ key ] = values.new;
+			} else {
+				restoredValues[ key ] = values.old;
+			}
+		} );
+
+		// Set at once.
+		settings.setExternalChange( restoredValues );
+
+		historyItem.set( 'status', isRedo ? 'not_applied' : 'applied' );
+
+		// Listen again
+		behavior.listenTo( settings, 'change', this.saveHistory );
 	},
 };
 
